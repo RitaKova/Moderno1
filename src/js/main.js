0709b270b415f6e529c5aeb9d5c46c5d5fd5c146
RitaKova <ritos96@ukr.net>
2020-10-29 21:08:52 +0200

    added second sectione

--- src/js/main.js
+++ src/js/main.js
@@ -50,51 +50,10 @@ $(function () {
       },
     ]
   });
-  $(".js-range-slider").ionRangeSlider({
-    type: "double",
-    min: 0,
-    max: 1000,
-    from: 0,
-    to: 600,
-    prefix: "$",
 
+  jQuery(document).ready(function () {
+    jQuery(".eTimer").eTimer({
+      etType: 2, etDate: "30.11.2020.17.10", etTitleSize: 19, etShowSign: 1, etSep: ":", etFontFamily: "Comic Sans MS", etTextColor: "#a3a3a3", etPaddingTB: 0, etPaddingLR: 0, etBackground: "#f6f0eb", etBorderSize: 0, etBorderRadius: 5, etBorderColor: "white", etShadow: " 0px 0px -2px 0px #333333", etLastUnit: 3, etNumberFontFamily: "Arial", etNumberSize: 26, etNumberColor: "white", etNumberPaddingTB: 0, etNumberPaddingLR: 7, etNumberBackground: "#ef7f1a", etNumberBorderSize: 0, etNumberBorderRadius: 5, etNumberBorderColor: "white", etNumberShadow: "inset 2px 0px 5px 0px rgba(0, 0, 0, 0.5)"
+    });
   });
-
-
-  $('.icon-th-list').on('click', function () {
-    $('.product-page__items .product__item').addClass('list');
-    $('.icon-th-list').addClass('active');
-    $('.icon-th-large').removeClass('active');
-  });
-
-  $('.icon-th-large').on('click', function () {
-    $('.product-page__items .product__item').removeClass('list');
-    $('.icon-th-large').addClass('active');
-    $('.icon-th-list').removeClass('active');
-  });
-  $('.menu__btn').on('click', function () {
-    $('.menu__list').slideToggle();
-  });
-
-  $('.header__btn-menu').on('click', function () {
-    $('.header__box').toggleClass('active');
-  });
-
-  $('input[type="file"], select').styler(); 1
-
-
-
-
-
-  $('.product-one__tabs .tab, .settings__tabs .tabs').on('click', function (event) {
-    var id = $(this).attr('data-id');
-    $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-    $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
-    $(this).addClass('active');
-    $('#' + id).addClass('active-tab').fadeIn();
-    return false;
-  });
-
-  var mixer = mixitup('.products__inner-box');
-
 });
